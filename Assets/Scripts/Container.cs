4b3026ee04940ad583252a08ea44b05d9aa1fe93
Yyang <xiyangyang@haishengyue.com>
2016-04-17 20:54:44 +0800

    list reuse

---
+++ Assets/Scripts/Container.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Container : MonoBehaviour
+{
+
+    public SUIWrapContent warp;
+List<string> stringList=new List<string>(); 
+	// Use this for initialization
+	void Start ()
+	{
+	    AddString();
+	    ShowList(stringList);
+
+	}
+	
+    void ShowList(List<string> stList )
+    {
+        warp.maxNum = stList.Count;
+        warp.first = 0;
+        warp.updateItemCallback = InitItem;
+        warp.wrapContent = true;
+        warp.Reset(false);
+    }
+
+    void InitItem(GameObject item, int index)
+    {
+        Item it = item.GetComponent<Item>();
+        it.SendString(stringList[index]);
+    }
+    void AddString()
+    {
+        stringList.Add("1");
+        stringList.Add("2");
+        stringList.Add("3");
+        stringList.Add("4");
+        stringList.Add("5");
+        stringList.Add("6");
+        stringList.Add("7");
+        stringList.Add("8");
+        stringList.Add("9");
+        stringList.Add("10");
+    }
+}
---
+++ Assets/Scripts/Item.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Item : MonoBehaviour
+{
+
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void SendString(string str)
+    {
+        if (str == null) return;
+        this.transform.Find("Label").GetComponent<UILabel>().text = str;
+    }
+}
